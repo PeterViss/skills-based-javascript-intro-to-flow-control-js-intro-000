db99dd616e899b7de1a927f8bfe64688512f8cf4
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-01-27 04:57:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -41,10 +41,10 @@ case 16:
   return ("You are a teenager")
 case 17:
   return ("You are a teenager")
-case 18: 
+case 18:
   return ("You are a teenager")
 case 19:
   return ("You are a teenager")
-default: 
+default:
   return ("You have an age")
 }
